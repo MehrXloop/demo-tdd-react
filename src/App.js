31001c118b4d228b6cf0a61ffdb3ac837553b346
MehrXloop <mehrunissa.pirwani@xloopdigital.com>
2023-04-19 04:46:40 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import TodoList from './components/TodoList';
-import './App.css';
-import { act } from '@testing-library/react';
-import TodoItem from './components/TodoItem';
+import React, { useState, useEffect } from "react";
+import TodoList from "./components/TodoList";
+import "./App.css";
+import { act } from "@testing-library/react";
+import TodoItem from "./components/TodoItem";
 
 function App() {
-
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
   const [saving, setSaving] = useState(false);
+  const [edit, setEditData] = useState();
+  const [date, setNewdate] = useState("");
   function onChange(e) {
     const value = e.target.value;
     setNewTodo(value);
   }
-  
+  function handleDate(e) {
+    const value = e.target.value;
+    setNewdate(value);
+  }
   function removeTodo(id) {
-    setTodos(todos.filter(t => t.id !== id));
+    setTodos(todos.filter((t) => t.id !== id));
   }
 
   function updateTodo(id) {
-      setLoading(true);
+    setLoading(true);
     const newList = todos.map((todoItem) => {
       if (todoItem.id === id) {
         const updatedItem = { ...todoItem, completed: !todoItem.completed };
@@ -31,6 +35,29 @@ function App() {
     setTodos(newList);
     setLoading(false);
   }
+  function handleEdit(i) {
+    const data = todos[i];
+    setNewTodo(data.newTodo);
+    setNewdate(data.date);
+    setEditData(i);
+  }
+  function handleUpdate(e) {
+    e.preventDefault();
+    const updatedTodo = {
+      ...todos[edit],
+      title: newTodo,
+      date: date,
+    };
+    const newList = todos.map((todoItem, index) => {
+      if (index === edit) {
+        return updatedTodo;
+      }
+      return todoItem;
+    });
+    setTodos(newList);
+    setNewTodo("");
+    setNewdate("");
+  }
 
   function addTodo(e) {
     e.preventDefault();
@@ -38,63 +65,74 @@ function App() {
       userId: 3,
       id: Math.floor(Math.random() * 10000) + 1,
       title: newTodo,
+      date: date,
       completed: false,
     };
-  
+
     setSaving(true);
     fetch(process.env.REACT_APP_API_URL, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(value),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
     })
       .then((response) => response.json())
       .then((result) => {
-        setTodos(todos.concat({ ...result, id: value.id }));
+        const newList = todos.concat({ ...result, id: value.id });
+        newList.sort((a, b) => a.title.localeCompare(b.title));
+        setTodos(newList);
         setSaving(false);
       });
   }
 
   useEffect(() => {
     async function fetchData() {
-      const result = await fetch(process.env.REACT_APP_API_URL).then((response) =>
-        response.json()
+      const result = await fetch(process.env.REACT_APP_API_URL).then(
+        (response) => response.json()
       );
-      act (()=>{
-        setTodos(result.slice(0, 5));
+      const sortedResult = result.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+      act(() => {
+        setTodos(sortedResult.slice(0, 5));
         setLoading(false);
-      })
-      
+      });
     }
     fetchData();
   }, []);
   return (
-  <div className="App">
-    <h1 className="header">My todo list</h1>
-    {loading ? (
-      'Loading'
-    ) : (
-      <TodoList todos={todos} removeHandler={removeTodo} updateTodo={updateTodo} />
-    )}
-
-    <div className="add-todo-form">
-      {saving ? (
-        'Saving'
+    <div className="App">
+      <header className="header">
+      <h1 className="header-heading">My Todo List</h1>
+      </header>
+      {loading ? (
+        "Loading"
       ) : (
-        <form onSubmit={addTodo}>
-          <input type="text" onChange={onChange} />
-          <button type="submit">Add new todo</button>
-        </form>
+        <TodoList
+          todos={todos}
+          handeleEdit={handleEdit}
+          removeHandler={removeTodo}
+          updateTodo={updateTodo}
+        />
       )}
-    </div>
-  </div>
-);
 
+      <div className="add-todo-form">
+        {saving ? (
+          "Saving"
+        ) : (
+          <form onSubmit={addTodo} className="form">
+            <input type="text" onChange={onChange} placeholder="write your todo here" />
+            <input type="date" onChange={handleDate}  />
+            <button type="submit">Add new todo</button>
+            {/* <button type="update" onClick={handleUpdate}> */}
+              {/* Update
+            </button> */}
+          </form>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default App;
-
-
-
-
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import styles from './TodoItem.module.css';
 
-const TodoItem = ({ todo, removeHandler, updateTodo }) => (
+const TodoItem = ({ todo, removeHandler, updateTodo,handleEdit }) => {
+  let previousDate = new Date(todo.date) <= new Date();
+  const styleTitle = previousDate ? styles.prevDate : '';
+  const styleDate = previousDate ? styles.prevDate : '';
+  return(
+
+ 
     <div className={styles.itemContainer}>
       <div>
+        
         <input
           type="checkbox"
           name={`checkbox-${todo.id}`}
@@ -16,9 +23,23 @@ const TodoItem = ({ todo, removeHandler, updateTodo }) => (
           onClick={() => updateTodo(todo.id)}
           className={todo.completed ? styles.completed : ''}
         >
-          {todo.title}
+         <h3 className={styleTitle}>Title: {todo.title}</h3> 
+        </label>
+        <label
+          htmlFor={`checkbox-${todo.id}`}
+          onClick={() => updateTodo(todo.id)}
+          className={todo.completed ? styles.completed : ''}
+        >
+          <p className={styleDate}>Due Date: {todo.date}</p>
         </label>
       </div>
+      {/* <button
+        className={styles.closeBtn}
+        data-testid={`close-btn-${todo.id}`}
+        onClick={() => handleEdit(todo.id)}
+      >
+        EDit
+      </button> */}
       <button
         className={styles.closeBtn}
         data-testid={`close-btn-${todo.id}`}
@@ -28,5 +49,5 @@ const TodoItem = ({ todo, removeHandler, updateTodo }) => (
       </button>
     </div>
   );
-  
+} 
 export default TodoItem;
